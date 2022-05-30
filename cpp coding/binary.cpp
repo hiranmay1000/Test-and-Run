aaed90408ee331e20810d1be703079f922e7d9a9
Seeker <hiranmay1000@gmail.com>
2022-05-30 17:42:30 +0530

    reverse an arr

--- cpp coding/binary.cpp
+++ cpp coding/binary.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-    char s[100];
-    cin >> s;
-    cout << s;
-    
+    int a[6] = {2, 8, 4, 5, 1, 3};
+    int n = 6;
+    int start = 0;
+    int end = n - 1;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        swap(a[start], a[end]);
+        start++;
+        end--;
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << a[i] << "  ";
+    }
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-  cout << "Just checking what it does" << endl;
-  return 0; 
+    char s[] = "Legend";
+    int start = 0;
+    int n = sizeof(s);
+    int end = n - 1;
+
+    for (size_t i = 0; s[i] != '\0'; i++)
+    {
+        swap(s[start], s[end]);
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << s[i];
+    }
+
+    return 0;
 }
