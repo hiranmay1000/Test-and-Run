4da6c26f7b69bd7add1c86e343103c5df8f3c51d
Max <hiranmay1000@gmail.com>
2023-01-29 13:20:33 +0530

    added remaining codes

--- keypad_mapping_combination.cpp
+++ keypad_mapping_combination.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 
-void backtrack(vector<string>& res, string sub, set<string> setres, int index, vector<int>& arr) {
+void backtrack(string& digits, vector<string>& res, string sub, int index, vector<string>& arr) {
     // base case
     if (index >= digits.size()) {
-        cout << sub << "  ";
+        res.push_back(sub);
         return;
     }
 
-    // Exclude
-    backtrack(digits, res, sub, setres, index + 1);
+    // Include
+    int val = (digits[index]) - '0';
+    string word = arr[val];
+    for (int i = 0; i < word.size(); i++)
+    {
+        sub.push_back(word[i]);
+        backtrack(digits, res, sub, index + 1, arr);
+        sub.pop_back();
+    }
 
-    // Include 
-    sub = +digits[index];
-    backtrack(digits, res, sub, setres, index + 1);
-
-    sub.pop_back();
 }
 
 vector<string> letterCombinations(string digits) {
@@ -33,15 +35,19 @@ vector<string> letterCombinations(string digits) {
 
 int main()
 {
-    string digits = "23";
+    string digits = "73";
     vector<string>  ans;
 
     ans = letterCombinations(digits);
 
-    for (auto it : ans) {
-        for (auto jt : it) {
-            cout << jt << ' ';
-        }cout << endl;
+    for (auto words : ans) {
+        cout << "[ ";
+        int j = 0;
+        for (auto letters : words) {
+            cout << letters ;
+            if(j++ < words.size() - 1)
+                cout << ',';
+        }cout << " ]" << endl;
     }
 
     return 0;
